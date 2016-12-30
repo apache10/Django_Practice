ec0e35800ec5738c906ee8467747cee97a63d8dc
gaurav <gagu12ec@gmail.com>
2016-12-30 13:11:32 +0530

    views are conected to database

--- website/music/views.py
+++ website/music/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse
+from models import Album
+
 
 # Create your views here.
 def index(request):
-	return HttpResponse("<h1>This is the music app homepage")
+	all_albums = Album.objects.all()
+	html=''
+	for album in all_albums:
+		url = '/music/'+str(album.id)+'/'
+		html += '<a href="'+ url +'">' +album.album_title+'</a><br>'
+	return HttpResponse(html)
+def details(request, album_id):
+	return HttpResponse("<h2>Details for Album id: "+ str(album_id)+"</h2>")
