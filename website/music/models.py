cade88070e02e66ec316853f1578e1baf1a0d4a3
gaurav <gagu12ec@gmail.com>
2016-12-30 12:14:48 +0530

    music app is migrated

--- website/music/models.py
+++ website/music/models.py
@@ -5,12 +5,12 @@ from django.db import models
 # Create your models here.
 #RED pk 1
 class Album(models.Model):
-	artist = models.CharField(max_lenght=250)
-	album_title =models.CharField(max_lenght=250)
-	genre = models.CharField(max_lenght=100)
-	album_logo = models.CharField(max_lenght=1000)
+	artist = models.CharField(max_length=250)
+	album_title =models.CharField(max_length=250)
+	genre = models.CharField(max_length=100)
+	album_logo = models.CharField(max_length=1000)
 
 class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
-	file_type = models.CharField(max_lenght=10)
-	song_title = models.CharField(max_lenght=250)	
+	file_type = models.CharField(max_length=10)
+	song_title = models.CharField(max_length=250)	
