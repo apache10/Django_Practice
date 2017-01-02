bdfb4bf7c997b212fb38b24d599db301764ad2dd
gaurav <gagu12ec@gmail.com>
2017-01-02 12:03:37 +0530

    hardcoded URLs has been removed

--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,6 @@
-from django.http import HttpResponse
-from django.http import Http404
+
 from models import Album
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
@@ -11,9 +10,7 @@ def index(request):
 	}
 	return render(request,'music/index.html',context)
 def details(request, album_id):
-	try:
-		album = Album.objects.get(pk=album_id)
-	except Album.DoesNotExist:
-		raise Http404("Album Does not exist")
+	#album = Album.objects.get(pk=album_id)
+	album =get_object_or_404(Album,pk=album_id)
 	return render(request,'music/detail.html',{'album':album
 	})
